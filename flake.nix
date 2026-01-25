0a3b474caa8824648a8c6b97800665ddb7431c28
Mitch Dzugan <mitchdzugan@gmail.com>
2026-01-25 09:34:27 -0600

    update deps

--- flake.nix
+++ flake.nix
@@ -1,9 +1,11 @@
 {
   inputs = {
     # lua-__.url = "path:/VOID/proj/__.lua";
-    # mitch-utils.url = "path:/VOID/proj/mitch-utils.nix";
-    mitch-utils.url = "github:mitchdzugan/mitch-utils.nix";
+    # lua-__.url = "path:/home/dz/Projects/__.lua";
     lua-__.url = "github:mitchdzugan/__.lua";
+    # mitch-utils.url = "path:/VOID/proj/mitch-utils.nix";
+    # mitch-utils.url = "path:/home/dz/Projects/mitch-utils.nix";
+    mitch-utils.url = "github:mitchdzugan/mitch-utils.nix";
   };
   outputs = inputs@{ mitch-utils, ... }: (mitch-utils.mkZnFnl inputs ./.);
 }
